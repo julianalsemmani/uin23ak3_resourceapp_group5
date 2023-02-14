8adb1f6831c334a6222017d534aaeaea6a128e22
Cecilie Seim <totilie8@gmail.com>
2023-02-14 17:24:19 +0100

    Setting up structure for sass

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
+import './css/main.css';
 import Header from "./components/Header";
 // import {Link} from 'react';
 import MainContent from "./components/MainContent";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header/>
       <nav>{/* <Link/> */}</nav>
       <MainContent />
-    </>
+    </div>
   );
 }
 
