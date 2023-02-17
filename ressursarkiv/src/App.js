a4d315707110ac440d53782724bcb76c4387ad5e
Cecilie Seim <totilie8@gmail.com>
2023-02-17 16:25:33 +0100

    Commented out temprary buttons before pull

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -3,19 +3,19 @@ import Header from "./components/Header";
 // import {Link} from 'react';
 import MainContent from "./components/MainContent";
 
-function App() {
-  return (
-    <div className="container">
-      <Header/>
-      <nav>
+/*    <nav>
         <button className='defaultStyle'>A</button>
         <button className='activeStyle'>A</button>
         <button className='testStyle'>A</button>
         <button className='test2Style'>A</button>
         <button className='test3Style'>A</button>
-        <button className='test4Style'>A</button>
-
-        {/* <Link/> */}</nav>
+        <button className='test4Style'>A</button></nav>
+ */
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+   
       <MainContent />
     </div>
   );
