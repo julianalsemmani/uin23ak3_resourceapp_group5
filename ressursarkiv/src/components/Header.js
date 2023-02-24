02b03194cc7d914b6cf39ae4a781b235949c578e
Cecilie Seim <totilie8@gmail.com>
2023-02-24 14:15:38 +0100

    Removed anchor tag

--- ressursarkiv/src/components/Header.js
+++ ressursarkiv/src/components/Header.js
@@ -2,9 +2,7 @@ export default function Header(){
     const title = "ressursarkiv";
     return (    
         <header className="pageHeader">
-            <a href="/">
                 <h1 className="headerTitle">{title}</h1>
-            </a>
         </header>   
    
     )
