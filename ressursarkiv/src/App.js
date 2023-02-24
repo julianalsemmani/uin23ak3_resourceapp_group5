619bf42e55ceb8e3ca9cb439a5bae5b381383943
Cecilie Seim <totilie8@gmail.com>
2023-02-24 14:04:37 +0100

    Remove app content for merge with layout branch

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -1,17 +1,10 @@
 import './css/main.css';
-import Header from "./components/Header";
-// import {Link} from 'react';
-import MainContent from "./components/MainContent";
-import Nav from "./components/Nav";
+
 
 function App() {
-  return (
-    <div className="container">
-      <Header/>
-      <Nav />
-      <MainContent />
-    </div>
-  );
+  return 
+    
+   
 }
 
 export default App;
